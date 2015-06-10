6f8081e67cc47c8e9561bd19124486732e81eb15
Geoffrey Vedernikoff <yefim323@gmail.com>
2015-06-09 17:52:37 -0700

    Sanitizes phone numbers

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var phone = require('phone');
 var router = express.Router();
 
 var peep = require('../lib/peep');
 
 router.post('/login', function(req, res) {
-  if (req.body && req.body.number && req.body.token) {
+  if (req.body && req.body.number && phone(req.body.number).length && req.body.token) {
+    req.body.number = phone(req.body.number)[0];
     peep.login(req.body)
       .then(function(result) {
         res.json(result);
@@ -18,7 +20,14 @@ router.post('/login', function(req, res) {
 });
 
 router.post('/register', function(req, res) {
-  if (req.body && req.body.number && req.body.token && req.body.contacts) {
+  if (req.body && req.body.number && phone(req.body.number).length && req.body.token && req.body.contacts) {
+    req.body.number = phone(req.body.number)[0];
+    // Clean up contacts
+    req.body.contacts = req.body.contacts.map(function(contact) {
+      contact.number = phone(contact.number).length ? phone(contact.number)[0] : '';
+      return contact;
+    }).filter(function(contact) { return contact.number !== '' && contact.number !== req.body.number; });
+
     peep.register(req.body)
       .then(function(result) {
         res.json(result);
